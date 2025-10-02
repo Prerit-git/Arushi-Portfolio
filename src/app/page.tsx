944ea17f8db955202c7a82f69769730517d59381
hustlers-tech <hustlerstech.com@gmail.com>
2025-10-02 14:27:36 +0530

    Test commit(color changed)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex justify-center h-screen items-center">
-      <h1 className="text-2xl font-semibold">Designed By Arushi</h1>
+      <h1 className="text-2xl font-semibold text-blue-500">Designed By Arushi</h1>
     </div>
   );
 }
