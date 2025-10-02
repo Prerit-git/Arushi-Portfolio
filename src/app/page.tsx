2d18d92337b93f6048741f14cad4cfb44dc9848b
hustlers-tech <hustlerstech.com@gmail.com>
2025-10-02 15:10:44 +0530

    Test deployment v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex justify-center h-screen items-center">
-      <h1 className="text-2xl font-semibold text-red-500">Designed By Arushi</h1>
+      <h1 className="text-3xl font-semibold text-red-500">Designed By Arushi</h1>
     </div>
   );
 }
