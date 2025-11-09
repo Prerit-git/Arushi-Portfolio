4e16e7b8b1129febc9e3e345f4acae7b9b0283ca
hustlers-tech <hustlerstech.com@gmail.com>
2025-11-09 20:44:04 +0530

    Added Footer

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,33 +1,47 @@
 import { motion } from "framer-motion";
+import Image from "next/image";
 import { MdOutlineMailOutline, MdOutlinePhone } from "react-icons/md";
 
 const Contact = () => {
+  const logoUrl = "/ArushiLogo.svg";
+  const mailLogo = "/mail.svg";
+  const linkedinLogo = "/linkedin.svg";
   return (
     <>
-      <section className="h-[30vh] md:h-[50vh] bg-black/50 flex flex-col w-full relative mb-0" id="contact">
-        <motion.div
-          className="flex flex-col md:flex-row justify-center items-start md:items-center gap-5 md:gap-20 pt-10 pl-10 md:pl-0"
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, ease: "easeOut" }}
-        >
-          <p className="text-2xl flex items-center gap-2 text-white">
-            <MdOutlineMailOutline />
-            <a href="mailto:arurajatmathur@gmail.com">arurajatmathur@gmail.com</a>
-          </p>
-          <p className="text-2xl flex items-center gap-2 text-white">
-            <MdOutlinePhone />
-            <a href="tel:7447604055">+91 7447604055</a>
-          </p>
-        </motion.div>
+      <section className="h-fit bg-[#FF0000] w-full flex flex-col px-[20px] md:px-[90px] pt-10 md:pt-20 pb-5 md:pb-15 gap-[24px]">
+        <p className="uppercase text-[20px] md:text-[24px] font-medium">Let's Connect</p>
+        <p className="font-extrabold text-[24px] md:text-[32px] leading-[110%]">
+          Looking for a product designer? <br />Drop me a line.
+        </p>
+        <p className="text-[20px] md:text-[24px] font-medium underline">
+          <a href="mailto:designedbyarushi@gmail.com">
+            designedbyarushi@gmail.com
+          </a>
+        </p>
 
-        <motion.div
-          className="flex justify-center"
-        >
-          <h1 className="text-[50px] lg:text-[200px] whitespace-nowrap font-semibold text-white/70 absolute bottom-0 uppercase">
-            Arushi Mathur
-          </h1>
-        </motion.div>
+        <div className="flex justify-between items-baseline">
+          <Image
+            src={logoUrl}
+            alt="Logo"
+            width={92}
+            height={92}
+            priority
+          />
+          <div className="flex gap-2">
+            <Image
+            src={mailLogo}
+            alt="mail-logo"
+            width={32}
+            height={32}
+            />
+            <Image
+            src={linkedinLogo}
+            alt="linkedin-logo"
+            width={32}
+            height={32}
+            />
+          </div>
+        </div>
       </section>
     </>
   );
