789c7f2a90f3e85eb8eee3cdeab2bb5a3154bb37
hustlers-tech <hustlerstech.com@gmail.com>
2025-11-09 20:50:00 +0530

    build errors resolved

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,6 +1,4 @@
-import { motion } from "framer-motion";
 import Image from "next/image";
-import { MdOutlineMailOutline, MdOutlinePhone } from "react-icons/md";
 
 const Contact = () => {
   const logoUrl = "/ArushiLogo.svg";
@@ -9,7 +7,7 @@ const Contact = () => {
   return (
     <>
       <section className="h-fit bg-[#FF0000] w-full flex flex-col px-[20px] md:px-[90px] pt-10 md:pt-20 pb-5 md:pb-15 gap-[24px]">
-        <p className="uppercase text-[20px] md:text-[24px] font-medium">Let's Connect</p>
+        <p className="uppercase text-[20px] md:text-[24px] font-medium">Let&apos;s Connect</p>
         <p className="font-extrabold text-[24px] md:text-[32px] leading-[110%]">
           Looking for a product designer? <br />Drop me a line.
         </p>
